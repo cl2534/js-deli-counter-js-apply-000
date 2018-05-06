292ec47af50c136ab9f55a405a0787b860f7291b
cl2534 <cl2534@nyu.edu>
2018-05-06 23:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ var katzDeliLine = [];
 
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
-  for (let i= 0; i < katzDeliLine.length; i++) {
-    if (name === katzDeliLine[i]) {
-      return ("Welcome, " + name + ". You are number " + (i+1) + " in line.")
-    }
-  }
+  return ("Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line.")
 }
+
+var took = 0;
+function takeANumber(katzDeliLine){
+  took++;
+  katzDeliLine.push(took);
+  return "Welcome. You're ticket number #" + took
+}
+
+// takeANumber(katzDeliLine) => "Welcome. You're ticket number #1" [1]
+// takeANumber(katzDeliLine) => "Welcome. You're ticket number #2" [1,2]
+
+// nowServing(katzDeliLine) => "Currently serving #1" [2]
+// nowServing(katzDeliLine) => "Currently serving #2" []
+// takeANumber(katzDeliLine) => "Welcome. You're ticket number #3" [3]
+
+instead putting name, put ticket number to the array. 
+
+
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) return ("There is nobody waiting to be served!");
